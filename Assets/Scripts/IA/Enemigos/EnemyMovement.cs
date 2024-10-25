ce4a7678e902c6737c25b2e0797a052b9cc7dee5
Saikou17 <a01783208@itesm.mx>
2024-10-24 20:23:27 -0600

    ai:Agregue NavMeshSurface

---
+++ Assets/Scripts/IA/Enemigos/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creamos una clase abstracta que defina metodos abstractos y metodos fijos para cada uno de los enemigos
+/// </summary>
+public abstract class EnemyMovement 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
