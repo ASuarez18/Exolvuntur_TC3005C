ccb297af8d5c7e542828b6ab12fdc5ea53d71c71
Alank99 <alan_hdz129@hotmail.com>
2024-11-05 19:54:46 -0600

    [ref:anim] blendtree

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //slider en el editor
+    [Range(-1,1)]
+    public float forward,side;
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Forward", forward);
+        anim.SetFloat("Side", side);
+
+        
+    }
+}
