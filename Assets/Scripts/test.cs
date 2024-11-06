c092b54eb55f0524a4fcfd12921559745adda792
Alank99 <alan_hdz129@hotmail.com>
2024-11-06 17:12:34 -0600

    [ref:anim]version 1 de la maquina de estados

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -8,6 +8,7 @@ public class test : MonoBehaviour
     //slider en el editor
     [Range(-1,1)]
     public float forward,side;
+    public int task;
     public Animator anim;
 
     // Update is called once per frame
@@ -15,6 +16,7 @@ public class test : MonoBehaviour
     {
         anim.SetFloat("Forward", forward);
         anim.SetFloat("Side", side);
+        anim.SetInteger("task", task);
 
         
     }
