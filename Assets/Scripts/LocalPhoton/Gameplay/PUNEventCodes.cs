0a9084109ed6f8e3add494994858ee72c60167bf
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2024-11-11 17:34:11 -0600

    multiplayer: Avances en PUNPlayerSpawner [Ref: NTWRK_CNTN]

---
+++ Assets/Scripts/LocalPhoton/Gameplay/PUNEventCodes.cs
@@ -0,0 +1,30 @@
+namespace LocalPhoton.Gameplay
+{
+    /// <summary>
+    /// Class that holds event codes for PUN
+    /// </summary>
+    public class PUNEventCodes
+    {
+        public enum EventCodes : byte
+        {
+            NewPlayer,
+            ListPlayers,
+            UpdateStat,
+            UpdateGameState
+        }
+
+        public enum GameStates : byte
+        {
+            Waiting,
+            Starting,
+            Playing,
+            Ending
+        }
+
+        public enum PlayerStats : byte
+        {
+            Kills
+            // More stats
+        }
+    }
+}
