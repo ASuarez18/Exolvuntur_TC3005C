103c46770837252b117f29ac80c56cf565c0908e
Benjamin Cruz <A01747811@tec.mx>
2024-11-12 18:21:58 -0600

    feat: Sistema de Inventario funcional [REF: INVTY_HUD]

--- Assets/Scripts/Player/Inventory/SlotSelector.cs
+++ Assets/Scripts/Player/Inventory/SlotSelector.cs
@@ -1,70 +1,98 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Playables;
 using UnityEngine;
-using Photon.Pun;
+using UnityEngine.UI;
+// using Photon.Pun;
 
 namespace Player.Inventory
 {
     /// <summary>
     /// Class in charge of the slot selector in the inventory of the player
     /// </summary>
-    public class SlotSelector : MonoBehaviourPunCallbacks
+    public class SlotSelector : MonoBehaviour //PunCallbacks
     {
         // Slot data structure to store the object model
-        [SerializeField]
-        public struct SlotData
+        [Serializable]
+        public struct UtilityData
         {
-            public GameObject utilityModel;
+            public GameObject utilityInstance;
+
+            public Sprite utilitySprite;
         }
 
-        // Current slot selected
-        public UtilityScriptableObject CurrentSlot {get; private set;}
+        // Current utility
+        public UtilityScriptableObject _currentUtility { get; private set; }
 
-        // All objects
+        // All utility
         [SerializeField] private UtilityScriptableObject[] _utilityData;
-        [SerializeField] private UtilityData[] _utilities;
+        [SerializeField] private UtilityData[] _utilities = new UtilityData[0];
+
+        // Parent object to store the utilities
+        public Transform _utilitiesParent;
 
         // Delegate for the slot change event
-        public delegate void OnSlotChange(UtilityScriptableObject utility);
-        public OnSlotChange onSlotChangeEvent;
+        public delegate void OnSlotChanged(UtilityScriptableObject utility);
+        public OnSlotChanged OnSlotChangedEvent;
 
-        // Current slot selected
+        private GameObject[] collectables;
+        private int currentCollectableIndex = 0;
+        // Current slot index
         public int _currentSlotIndex;
+
+        // Max slots
+        private const int MAX_SLOTS = 3;
+
+        public Image[] hudSlots;
 
         /// <summary>
         /// Method to enable the component
         /// </summary>
         void OnEnable()
         {
-            // Photon
             // enabled = photonView.IsMine;
             ChangeSlot();
         }
 
+
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             // Enables or disables this component based on the player's ownership
             //enabled = photonView.IsMine;
 
             // Change the weapon in all clients
             //photonView.RPC(nameof(PUNChangeUtility), RpcTarget.All, _currentSlotIndex);
+
+            collectables = GameObject.FindGameObjectsWithTag("Collectable");
         }
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CollectObject();
+            }
+
             // Change the slot with mouse wheel
             if (Input.GetAxis("Mouse ScrollWheel") > 0f)
             {
                 _currentSlotIndex++;
                 //photonView.RPC(nameof(PUNChangeUtility), RpcTarget.All, _currentSlotIndex);
+                if (_currentSlotIndex >= _utilities.Length)
+                {
+                    _currentSlotIndex = 0;
+                }
+                ChangeSlot();
             }
             else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
             {
                 _currentSlotIndex--;
+                if (_currentSlotIndex < 0)
+                {
+                    _currentSlotIndex = _utilities.Length - 1;
+                }
                 //photonView.RPC(nameof(PUNChangeUtility), RpcTarget.All, _currentSlotIndex);
+                ChangeSlot();
             }
 
             // Select the slot with the number keys
@@ -78,34 +106,100 @@ namespace Player.Inventory
             }
         }
 
+        private void CollectObject()
+        {
+            if (collectables.Length == 0) return;
+
+            if (_utilities.Length >= MAX_SLOTS)
+            {
+                Debug.Log("No more slots available.");
+                return;
+            }
+
+            GameObject currentCollectable = collectables[currentCollectableIndex];
+            SpriteRenderer _spriteRenderer = currentCollectable.GetComponent<SpriteRenderer>();
+
+            if (_spriteRenderer == null)
+            {
+                Debug.LogError($"No SpriteRenderer found on {currentCollectable.name}");
+                return;
+            }
+
+            Array.Resize(ref _utilities, _utilities.Length + 1);
+            _utilities[_utilities.Length - 1] = new UtilityData
+            {
+                utilityInstance = currentCollectable,
+                utilitySprite = currentCollectable.GetComponent<SpriteRenderer>().sprite
+            };
+
+            // Emparenta el objeto con el objeto padre
+            currentCollectable.transform.SetParent(_utilitiesParent);
+
+            // Actualiza el índice para el siguiente objeto
+            currentCollectableIndex = (currentCollectableIndex + 1) % collectables.Length;
+
+            UpdateHUD();
+        }
+
         /// <summary>
         /// Method to change the slot of the inventory
         /// </summary>
         private void ChangeSlot()
         {
+            if (_utilities.Length == 0) return;
+
             if (_currentSlotIndex < 0)
             {
-                _currentSlotIndex = _utilityData.Length - 1;
+                _currentSlotIndex = _utilities.Length - 1;
             }
-            else if (_currentSlotIndex >= _utilityData.Length)
+            else if (_currentSlotIndex >= _utilities.Length)
             {
                 _currentSlotIndex = 0;
             }
 
-            // Notify the change of the slot to the player
-            onSlotChangeEvent?.Invoke(_utilityData[_currentSlotIndex]);
+            // Lógica de cambio de ranura
+            OnSlotChangedEvent?.Invoke(_utilityData[_currentSlotIndex]);
 
-            // Visually change the slot
             foreach (var utility in _utilities)
             {
-                utility.utilityModel.SetActive(false);
+                utility.utilityInstance.SetActive(false);
             }
 
-            _utilities[_currentSlotIndex].utilityModel.SetActive(true);
+            _utilities[_currentSlotIndex].utilityInstance.SetActive(true);
+
+            UpdateHUD();
         }
 
+        private void UpdateHUD()
+        {
+            for (int i = 0; i < hudSlots.Length; i++)
+            {
+                if (i < _utilities.Length)
+                {
+                    hudSlots[i].sprite = _utilities[i].utilitySprite;
+                    hudSlots[i].enabled = true;
+
+                    if (i == _currentSlotIndex)
+                    {
+                        hudSlots[i].color = Color.white;
+                        hudSlots[i].rectTransform.localScale = Vector3.one;
+                    }
+                    else
+                    {
+                        hudSlots[i].color = Color.gray;
+                        hudSlots[i].rectTransform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                    }
+                }
+                else
+                {
+                    hudSlots[i].enabled = false;
+                }
+            }
+        }
+
+
         // Get the data of the current slot
-        public SlotData GetCurrentSlotData()
+        public UtilityData GetCurrentSlotData()
         {
             return _utilities[_currentSlotIndex];
         }
@@ -113,7 +207,7 @@ namespace Player.Inventory
         /// <summary>
         /// Method to update the slot selector in all clients
         /// </summary>
-        [PunRPC]
+        // [PunRPC]
         public void PUNChangeUtility(int slotIndex)
         {
             _currentSlotIndex = slotIndex;
