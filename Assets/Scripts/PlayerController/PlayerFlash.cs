2cec38452f0f4bb836be9e8d295016fc40f50d5c
Ramiro González Dávila <a01422501@tec.mx>
2024-11-19 21:30:35 -0600

    fix: adding view to prefabs for animations

--- Assets/Scripts/PlayerController/PlayerFlash.cs
+++ Assets/Scripts/PlayerController/PlayerFlash.cs
@@ -1,14 +1,17 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFlash : MonoBehaviour
+public class PlayerFlash : MonoBehaviourPunCallbacks
 {
    [SerializeField] private GameObject flashLight;
 
     // Update is called once per frame
     void Update()
     {
+        if (!photonView.IsMine) return;
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             flashLight.SetActive(!flashLight.activeSelf);
